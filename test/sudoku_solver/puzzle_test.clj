0cab8980f369b0b7034c117718af9e82b8b2cc22
Daniel Fischer <d.fischer874@gmail.com>
2015-01-19 19:45:28 -0500

    Wrote a test that ensures assign-number-in-row does nothing if it is unable to assign the number in the row; changed test case relating to assigning a number in a row from invoking assign-number-in-quadrant to invoking assign-number-in-row.

--- test/sudoku_solver/puzzle_test.clj
+++ test/sudoku_solver/puzzle_test.clj
@@ -264,27 +264,7 @@
                                        [\9  nil nil \1  nil nil \2  nil nil]
                                        [\4  \9  \5  \8  \2  \7  nil nil \6 ]
                                        [nil nil nil \6  \1  \5  \9  \4  \7 ]
-                                       [\7  \1  \6  \9  \4  \3  nil nil nil]] '(2 0) \3))))
-
-  (testing "assigns number 5 in quadrant 2 by observing that 5 is the only number left of the three to complete the row which can be assigned to coordinate (0,2)"
-    (is (= [[nil \2  \4  \1  nil \5  \3  \7  \9 ]
-            [\3  \1  nil nil nil \9  nil \6  \2 ]
-            [nil \7  nil \2  nil \3  nil \4  \1 ]
-            [\1  \6  \5  \8  \3  \2  \4  \9  \7 ]
-            [nil \9  \3  nil nil nil \6  \2  nil]
-            [nil \4  \2  \9  nil \6  \1  \3  nil]
-            [nil \3  nil \5  nil \8  \2  \1  nil]
-            [\2  \5  nil \6  nil nil \7  \8  \3 ]
-            [nil \8  nil \3  \2  nil \9  \5  nil]]
-           (assign-number-in-quadrant [[nil \2  \4  \1  nil nil \3  \7  \9 ]
-                                       [\3  \1  nil nil nil \9  nil \6  \2 ]
-                                       [nil \7  nil \2  nil \3  nil \4  \1 ]
-                                       [\1  \6  \5  \8  \3  \2  \4  \9  \7 ]
-                                       [nil \9  \3  nil nil nil \6  \2  nil]
-                                       [nil \4  \2  \9  nil \6  \1  \3  nil]
-                                       [nil \3  nil \5  nil \8  \2  \1  nil]
-                                       [\2  \5  nil \6  nil nil \7  \8  \3 ]
-                                       [nil \8  nil \3  \2  nil \9  \5  nil]] '(0 1) \5)))))
+                                       [\7  \1  \6  \9  \4  \3  nil nil nil]] '(2 0) \3)))))
 
 (deftest assign-number-in-row-test
   (testing "assigns-number-in-row-when-that-number-completes-the-row"
@@ -305,7 +285,45 @@
                                                 [nil nil nil nil nil nil nil nil nil]
                                                 [nil nil nil nil nil nil nil nil nil]
                                                 [nil nil nil nil nil nil nil nil nil]
-                                                [nil nil nil nil nil nil nil nil nil]] 1 \1)))))
+                                                [nil nil nil nil nil nil nil nil nil]] 1 \1))))
+  (testing "does nothing if it cannot assign a number in row"
+    (is (= [[nil nil nil nil nil nil nil nil nil]
+            [\5  \6  \2  \8  nil \4  \9  nil \7 ]
+            [nil nil nil nil nil nil nil nil nil]
+            [nil nil nil nil nil nil nil nil nil]
+            [nil nil nil nil nil nil nil nil nil]
+            [nil nil nil nil nil nil nil nil nil]
+            [nil nil nil nil nil nil nil nil nil]
+            [nil nil nil nil nil nil nil nil nil]
+            [nil nil nil nil nil nil nil nil nil]]
+           (assign-number-in-row               [[nil nil nil nil nil nil nil nil nil]
+                                                [\5  \6  \2  \8  nil  \4  \9 nil \7 ]
+                                                [nil nil nil nil nil nil nil nil nil]
+                                                [nil nil nil nil nil nil nil nil nil]
+                                                [nil nil nil nil nil nil nil nil nil]
+                                                [nil nil nil nil nil nil nil nil nil]
+                                                [nil nil nil nil nil nil nil nil nil]
+                                                [nil nil nil nil nil nil nil nil nil]
+                                                [nil nil nil nil nil nil nil nil nil]] 1 \1))))
+  (testing "assigns number 5 in quadrant 2 by observing that 5 is the only number left of the three to complete the row which can be assigned to coordinate (0,2)"
+    (is (= [[nil \2  \4  \1  nil \5  \3  \7  \9 ]
+            [\3  \1  nil nil nil \9  nil \6  \2 ]
+            [nil \7  nil \2  nil \3  nil \4  \1 ]
+            [\1  \6  \5  \8  \3  \2  \4  \9  \7 ]
+            [nil \9  \3  nil nil nil \6  \2  nil]
+            [nil \4  \2  \9  nil \6  \1  \3  nil]
+            [nil \3  nil \5  nil \8  \2  \1  nil]
+            [\2  \5  nil \6  nil nil \7  \8  \3 ]
+            [nil \8  nil \3  \2  nil \9  \5  nil]]
+           (assign-number-in-row      [[nil \2  \4  \1  nil nil \3  \7  \9 ]
+                                       [\3  \1  nil nil nil \9  nil \6  \2 ]
+                                       [nil \7  nil \2  nil \3  nil \4  \1 ]
+                                       [\1  \6  \5  \8  \3  \2  \4  \9  \7 ]
+                                       [nil \9  \3  nil nil nil \6  \2  nil]
+                                       [nil \4  \2  \9  nil \6  \1  \3  nil]
+                                       [nil \3  nil \5  nil \8  \2  \1  nil]
+                                       [\2  \5  nil \6  nil nil \7  \8  \3 ]
+                                       [nil \8  nil \3  \2  nil \9  \5  nil]] 0 \5)))))
 
   (deftest lateral-sibling-quadrants-test
     (testing "lateral-sibling-quadrants"
